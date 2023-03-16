841a60087a06cca296cc372599374d0ff02cd9af
pavankudipudi <121846255+pavankudipudi@users.noreply.github.com>
2023-03-16 11:52:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, nagendra01!\n');
+  res.end('Hello, i am wroking!\n');
 });
 
 server.listen(port, hostname, () => {
